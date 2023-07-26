0229744032dfef74cc5e310a180f52bd17e8e4d9
nimanoro <nimanourozy@gmail.com>
2023-07-25 23:55:41 -0700

    Phase 1 "Saving option added + test"

--- src/test/persistent/JsonReaderTest.java
+++ src/test/persistent/JsonReaderTest.java
@@ -1,6 +1,8 @@
 package persistent;
 
 import model.League;
+import model.Manager;
+import model.Team;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import persistence.JsonReader;
@@ -18,21 +20,26 @@ public class JsonReaderTest {
     }
     @Test
     public void TestReadManager() throws IOException {
-        assertEquals(jsonReader.readManager().getCoin(),75);
+        assertEquals(jsonReader.readManager().getCoin(),300);
 
 
     }
 
     @Test
     public void TestReadFixture() throws IOException {
-        assertEquals(jsonReader.readFixture().getWeek(),2);
+        Manager manager = jsonReader.readManager();
+        Team team = jsonReader.readTeam(manager);
+        assertEquals(jsonReader.readFixture(team).getWeek(),5);
+
     }
 
     @Test
     public void TestReadLeague() throws IOException {
-        League league = jsonReader.readLeague();
+        Manager manager = jsonReader.readManager();
+        Team team = jsonReader.readTeam(manager);
+        League league = jsonReader.readLeague(team);
         league.sortTeams();
-        assertEquals(league.getTeams().get(0).getName(), "Barcelona");
+        assertEquals(league.getTeams().get(0).getName(), "Dream Team");
 
     }
 }
