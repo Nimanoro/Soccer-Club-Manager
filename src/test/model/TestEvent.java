0fe95cd168f53a74344ca0cb2d665c1ce502218b
nimanoro <nimanourozy@gmail.com>
2023-08-09 17:54:43 -0700

    Phase 4 test?

--- src/test/model/TestEvent.java
+++ src/test/model/TestEvent.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for the Event class
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestEvent {
     private Event e;
     private Date d;
+    private Event m;
 
     //NOTE: these tests might fail if time at which line (2) below is executed
     //is different from time that line (1) is executed.  Lines (1) and (2) must
@@ -23,11 +24,14 @@ public class TestEvent {
     public void runBefore() {
         d = Calendar.getInstance().getTime();   // (2)
         e = new Event("n");   // (1)
+        m = new Event("F");
     }
 
     @Test
     public void testEvent() {
         assertEquals("n", e.getDescription());
+        assertTrue(e.equals(e));
+        assertFalse(e.equals(m));
     }
 
     @Test
